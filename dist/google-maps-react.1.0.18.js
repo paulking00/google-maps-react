ef6e3ececaf8edb52d9d409301d581915652c85b
pking <p.king@kasasi.de>
2016-09-07 17:00:39 +0200

    build properly

--- dist/google-maps-react.1.0.18.js
+++
@@ -1,1 +0,0 @@
-!function(r){function t(n){if(o[n])return o[n].exports;var e=o[n]={exports:{},id:n,loaded:!1};return r[n].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=r,t.c=o,t.p="/",t(0)}([function(r,t,o){r.exports=o(1)},function(r,t,o){"use strict";r.exports=o(!function(){var r=new Error('Cannot find module "./dist/index"');throw r.code="MODULE_NOT_FOUND",r}())}]);
